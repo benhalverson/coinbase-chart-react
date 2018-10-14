5951c1233fe820e2776b37d585ca65f38e278f70
saravanan10393 <saravanan.10393@gmail.com>
2018-10-14 17:25:37 +0530

    enzyme setup added

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Enzyme, {mount} from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
 import App from './App';
 
+Enzyme.configure({
+  adapter: new Adapter()
+});
+
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+  let app = mount(<App />);
+  expect(app).toBeDefined();
 });
